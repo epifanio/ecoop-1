53d1c5ea4c95e61a716a857cb0701004545b34e6
epifanio <epiesasha@me.com>
2014-07-01 21:40:03 -0400

    try to add metadata directly at the notebook level

--- pyecoop/lib/ecoop/cf.py
+++ pyecoop/lib/ecoop/cf.py
@@ -116,8 +116,8 @@ class cfData():
                         ]
                     }
                 }
-                display('nao_get - metadata saved', metadata={'ecoop_prov': jsonld})
-                display(Javascript("IPython.notebook.metadata.ecoop_prov.nao_get = {}".format(jsonld)))
+                #display('nao_get - metadata saved', metadata={'ecoop_prov': jsonld})
+                display(Javascript("IPython.notebook.metadata.ecoop_prov['nao_get'] = %s" % jsonld))
 
 
             return naodata
@@ -196,8 +196,8 @@ class cfData():
                         ]
                     }
                 }
-                display('nin_get - metadata saved', metadata={'ecoop_prov': jsonld})
-                display(Javascript("IPython.notebook.metadata.ecoop_prov.nin_get = {}".format(jsonld)))
+                #display('nin_get - metadata saved', metadata={'ecoop_prov': jsonld})
+                display(Javascript("IPython.notebook.metadata.ecoop_prov['nin_get'] = %s" % jsonld))
             return nin_anomalies
         except IOError:
             print(
@@ -284,7 +284,8 @@ class cfData():
                 }
                 #display('amo_get - metadata saved', metadata={'ecoop_prov': jsonld})
                 print(jsonld)
-                display(Javascript("IPython.notebook.metadata.ecoop_prov.amo_get = {}".format(jsonld)))
+                display(Javascript("IPython.notebook.metadata.ecoop_prov['amo_get'] = %s" % jsonld))
+                #display(Javascript("IPython.notebook.metadata.ecoop_prov.amo_get = {}".format(jsonld)))
             return amodata
         except:
             print(
